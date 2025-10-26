6b2f7d90c6cfe89236352a3e9841443e5c131ab6
aztec-dev <aztech.dev1@gmail.com>
2025-10-26 15:56:12 +1000

    fix: minor code cleaning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,43 @@
-import { Stack } from "expo-router";
+import { useColorScheme } from '@/app-example/hooks/use-color-scheme.web';
+import { useFonts } from '@expo-google-fonts/inter/useFonts';
+import { Playfair_400Regular } from '@expo-google-fonts/playfair/400Regular';
+import { Roboto_500Medium, Roboto_800ExtraBold } from '@expo-google-fonts/roboto';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import './global.css';
+SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const [loaded, error] = useFonts({
+    Playfair_400Regular,
+    Roboto_500Medium,
+    Roboto_800ExtraBold,
+  });
+
+  useEffect(()=> {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false , title: 'Landing'}} />
+      <Stack.Screen name="screens/SignIn" options={{ headerShown: true, title: 'Sign In' }} />
+      <Stack.Screen name="screens/SignUp" options={{ headerShown: true, title: 'Sign Up' }} />
+    </Stack>
+    <StatusBar style="auto" />
+  </ThemeProvider>
+
+  );
 }
