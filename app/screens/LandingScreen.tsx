5616804e92bcd85026e803de79ebf847cd0baa61
aztec-dev <aztech.dev1@gmail.com>
2025-10-26 15:53:47 +1000

    fix: integrated nativewind instead of styling from scratch

--- app/screens/LandingScreen.tsx
+++ app/screens/LandingScreen.tsx
@@ -1,5 +1,3 @@
-import { useFonts } from '@expo-google-fonts/inter/useFonts'
-import { Playfair_300Light } from '@expo-google-fonts/playfair/300Light'
 import { useRouter } from 'expo-router'
 import React, { useState } from 'react'
 import {
@@ -7,137 +5,67 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
+  View
 } from 'react-native'
-import Animated, { FadeInUp } from 'react-native-reanimated'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HeroSection from '../components/HeroSection'
+import '../global.css'
 
 export default function Auth() {
   const [loading, setLoading] = useState(false)
   const router = useRouter();
-
-  let fonts = useFonts({
-    Playfair_300Light
-  })
+  // const colorScheme = useColorScheme();
+  // const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  // const themeButtonStyle = colorScheme === 'light' ? styles.buttonLightTheme : styles.buttonDarkTheme;
 
   return (
-    <View style={[styles.container]}>
-      <View style={[styles.blueHalfContainer, {alignItems: 'center'}]}>
-        <Text style={{marginTop: '25%'}}>Temp logo here hehe</Text>
-      </View>
-      <View>
-
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Animated.View entering={FadeInUp.duration(500).delay(200)}>
-          <Text style={[styles.bold, { fontFamily: "Roboto", fontSize: 28, textTransform: 'capitalize', marginTop: 20, }]}>
+    <SafeAreaView className='flex-auto flex-col container mx-auto'>
+      <View className='bg-primary h-[60px] absolute top-0 left-0 right-0 z-10'></View>
+      <HeroSection />
+      <View className='py-5 px-4'>
+          <Text className='text-[28px]' style={{fontFamily: 'Roboto_800ExtraBold'}}>
           Bringing Christians together through prayer
         </Text>
-        <Text style={[styles.light, { fontFamily: "Playfair_300Light", fontSize: 22, marginTop: 10,}]}>
+        <Text className='text-[22px]' style={{fontFamily: 'Playfair_400Regular'}}>
           “For where two or three are gathered together in My name, 
           I am there in the midst of them.” - Matt. 18:20
         </Text>
-        </Animated.View>
       </View>
-      <View style={[styles.flexDisplay]}>
+
+      {/* sign in/up buttons */}
+      <View className='flex flex-row justify-center gap-6 mt-20'>
         <TouchableOpacity
-          style={[styles.button, loading && styles.disabled]}
+          className='w-48 h-14 flex-initial bg-primary items-center justify-center rounded-xl'
           onPress={() => router.navigate('/screens/SignIn')}
           disabled={loading}
         >
           {loading ? (
             <ActivityIndicator color="#fff" />
           ) : (
-            <Text style={styles.buttonText}>Sign in</Text>
+            <Text className='text-white'>Sign in</Text>
           )}
         </TouchableOpacity>
-                <TouchableOpacity
-          style={styles.button}
+          <TouchableOpacity
+          className='w-48 h-14 flex-initial bg-primary items-center justify-center rounded-xl'
           onPress={() => router.navigate("/screens/SignUp")}
         >
-          <Text style={styles.buttonText}>Go to Sign up</Text>
+          <Text className='text-white'>Sign up</Text>
         </TouchableOpacity>
       </View>
 
       <View style={[styles.verticallySpaced]}>
 
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  blueHalfContainer: {
-    backgroundColor: '#5B7E98',
-    width: '100%',
-    height: '45%',
-    borderEndStartRadius: 25,
-    borderEndEndRadius: 25,
-    shadowColor: "#000",         
-    shadowOpacity: 0.2,          
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 2,
-    elevation: 3,
-  },
-  whiteHalfContainer: {
-    backgroundColor: "#fff",
-  },
-  container: {
-    flex: 1,
-    marginTop: 0,
-    padding: 0,
-  },
   verticallySpaced: {
     paddingTop: 8,
     paddingLeft: 12,
     paddingRight: 12,
     paddingBottom: 8,
     alignSelf: 'stretch',
-  },
-  mt20: {
-    marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 12,
-    fontSize: 16,
-  },
-  button: {
-    backgroundColor: '#5B7E98',
-    flex: 1,
-    height: 60,
-    borderRadius: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 5,
-    marginTop: 'auto',
-    marginBottom: 40,
-    shadowColor: "#000",         
-    shadowOpacity: 0.2,          
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 2,
-    elevation: 3,
-  },
-  disabled: {
-    opacity: 0.6,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: '600',
-    fontFamily: 'Roboto',
-    fontSize: 18,
-  },
-  light: {
-    fontWeight: '300',
-  },
-  bold: {
-    fontWeight: '800',
-  },
-  flexDisplay: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 12,
-  },
+  }
 })
