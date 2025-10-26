4f6b9ee9234daf66fbe0fa4f42c0080be2ef7947
aztec-dev <aztech.dev1@gmail.com>
2025-10-26 15:55:33 +1000

    added custom color test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/index.tsx", "./app/components/**/*.{js,jsx,ts,tsx}", "./app/screens/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: { extend: {
+    colors: {
+      primary: '#5B7E98',
+    },
+  }, 
+},
   plugins: [],
-}
-
+};
