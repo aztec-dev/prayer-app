d65f74d87889d116db950c36c9f5e6303ec2f307
aztec-dev <aztech.dev1@gmail.com>
2025-10-26 16:11:51 +1000

    fix: adjusted temp text

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,8 +1,8 @@
 import { Text, View } from "react-native"
 export default function HeroSection() {
     return (
-        <View className="bg-primary w-full h-[50%] rounded-b-3xl">
-            <Text>Hero section</Text>
+        <View className="bg-primary w-full h-[50%] rounded-b-3xl items-center justify-center">
+            <Text className="text-white">Hero section</Text>
         </View>
     )
 }
