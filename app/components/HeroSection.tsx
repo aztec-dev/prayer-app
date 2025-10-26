7730c260ef259fae1aa711c96389b34bd88f4cef
aztec-dev <aztech.dev1@gmail.com>
2025-10-26 15:54:06 +1000

    init

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native"
+export default function HeroSection() {
+    return (
+        <View className="bg-primary w-full h-[50%] rounded-b-3xl">
+            <Text>Hero section</Text>
+        </View>
+    )
+}
